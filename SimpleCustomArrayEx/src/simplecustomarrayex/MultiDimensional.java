28ca4710e63c95c60d11bf3af5ccedc9ff4f040f
muhammadmustafa <muhammadmustafa@DESKTOP-PHR9C7R>
2023-02-05 20:19:45 +0500

    Updated

--- SimpleCustomArrayEx/src/simplecustomarrayex/MultiDimensional.java
+++ SimpleCustomArrayEx/src/simplecustomarrayex/MultiDimensional.java
@@ -1,12 +1,42 @@
 package simplecustomarrayex;
 
+import java.util.Scanner;
+
 public class MultiDimensional {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int arr1,arr2;
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter The Number of Rows = ");
+		arr1 = s.nextInt();
+		System.out.println("Enter The Number of Colums = ");
+		arr2  = s.nextInt();
+		int arr[][]  = new int[arr1][arr2]; 
+		System.out.println("Enter The Number of Values = ");
+		for(int i = 0; i<arr1; i++) 
+		{
+		
+			for(int j = 0; j<arr2; j++) {
+				
+				arr[i][j] = s.nextInt();
+				
+			}
+			
+		}
 		
 		
+		for(int i = 0; i<arr1; i++) 
+		{
+		
+			for(int j = 0; j<arr2; j++) {
+				
+				System.out.print(arr[i][j]+" " );
+			}
+			
+			System.out.println();
+		}
 		
 	}
 
